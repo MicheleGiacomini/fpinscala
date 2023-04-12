e65ef930dfe4a0b3583f23ce973e225504ea6d9f
Michele Giacomini <michele.giacomini91@gmail.com>
2023-04-12 21:47:18 +0200

    More for comprehensions

--- src/main/scala/fpinscala/answers/errorhandling/Option.scala
+++ src/main/scala/fpinscala/answers/errorhandling/Option.scala
@@ -68,7 +68,10 @@ object Option:
   // a bit later in the chapter we'll learn nicer syntax for
   // writing functions like this
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-    a.flatMap(aa => b.map(bb => f(aa, bb)))
+    for
+      aa <- a
+      bb <- b
+    yield f(aa, bb)
 
   /* Here's an explicit recursive version: */
   def sequence[A](as: List[Option[A]]): Option[List[A]] =
